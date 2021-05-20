b88900a7c5905e4d79e5a79b73abc6ba39da1f5c
Сергей Осокин <rossemir11@gmail.com>
2021-05-20 18:11:07 +0300

    Improvements_By_Comments

--- Assets/Scripts/LinearColorChanger.cs
+++ Assets/Scripts/LinearColorChanger.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class LinearColorChanger : MonoBehaviour
 {
-    [HideInInspector] public SpriteRenderer Target;    
-
     [SerializeField] private float _duration;
     [SerializeField] private Color _targetColor;
-
+    
+    private SpriteRenderer Target;
     private float _runningTime;
     private Color _startColor;
-    
 
     private void Start()
     {
         Target = GetComponent<SpriteRenderer>();
         _startColor = Target.color;
     }
-    void Update()
+    private void Update()
     {
         if(_runningTime <= _duration)
         {
